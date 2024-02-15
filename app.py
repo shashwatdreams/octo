3d8680ef9bcecf009ba36c093e8191da9a4220fa
Shashwat Sharma <68554481+shashwat73@users.noreply.github.com>
2024-02-15 03:19:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from openai import OpenAI
 import streamlit as st
 import hmac
 
+#about
+
+st.set_page_config(
+    menu_items={
+        'About': "# check it"
+    }
+)
+
 # Function to check password
 def check_password():
     def password_entered():
@@ -21,36 +29,55 @@ def check_password():
         st.error("😕 Password incorrect")
     return False
 
+
+
 if not check_password():
     st.stop()
 
+# Define the model mapping
+model_mapping = {
+    "GPT-3.5-Turbo": "gpt-3.5-turbo-0125",
+    "GPT-4": "gpt-4"
+}
+
+# Create columns for layout
 col1, col2 = st.columns([3, 1])
 with col1:
     st.title("octo")
 with col2:
-    model_selection = st.selectbox("", ["gpt-3.5-turbo", "gpt-4"], key="openai_model")
+    # Use the model_mapping keys for the selectbox display options
+    model_selection = st.selectbox(list(model_mapping.keys()), key="openai_model")
 
+# Initialize the OpenAI client with the API key
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
+# Initialize chat messages in session state if not present
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Display previous messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+# Input for new messages
 if prompt := st.chat_input("Message Octo..."):
+    # Append the new user message to the session state
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
+    # Handle the assistant response
     with st.chat_message("assistant"):
+        # Use the model_mapping to get the specific model identifier
+        selected_model = model_mapping[model_selection]
         stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
+            model=selected_model,
             messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
             stream=True,
         )
         response = st.write_stream(stream)
+    # Append the assistant's response to the session state
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 # Hide Streamlit's default UI elements
