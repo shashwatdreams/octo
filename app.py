ba09a5c202b1cd0e7ec33819baa736e26b1c236d
Shashwat Sharma <68554481+shashwat73@users.noreply.github.com>
2024-08-31 14:23:56 -0400

    updating models

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ st.set_page_config(
     }
 )
 
-
 def check_password():
     def password_entered():
         if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
@@ -32,10 +31,8 @@ if not check_password():
     st.stop()
 
 model_mapping = {
-    "GPT-3.5": "gpt-3.5-turbo",
-    "GPT-4": "gpt-4-turbo-preview",
+    "GPT-4o": "gpt-4o-mini",
     "Google Gemini": "google-gemini",
-    "Claude 3.5": "claude-3.5-sonnet"
 }
 
 st.markdown("""
@@ -50,15 +47,13 @@ with col1:
 with col2:
     model_selection = st.selectbox("", list(model_mapping.keys()), key="openai_model")
 
-if model_selection in ["GPT-3.5", "GPT-4"]:
+if model_selection == "GPT-4o-Mini":
     client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 elif model_selection == "Google Gemini":
     gen_ai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
     model = gen_ai.GenerativeModel('gemini-pro')
     if "chat_session" not in st.session_state:
         st.session_state.chat_session = model.start_chat(history=[])
-elif model_selection == "Claude 3.5":
-    client = anthropic.Client(api_key=st.secrets["ANTHROPIC_API_KEY"])
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -72,7 +67,7 @@ if prompt := st.chat_input("Message Octo..."):
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    if model_selection in ["GPT-3.5", "GPT-4"]:
+    if model_selection == "GPT-4o-Mini":
         with st.chat_message("assistant"):
             selected_model = model_mapping[model_selection]
             stream = client.chat.completions.create(
@@ -87,15 +82,6 @@ if prompt := st.chat_input("Message Octo..."):
             gemini_response = st.session_state.chat_session.send_message(prompt)
             st.markdown(gemini_response.text)
             st.session_state.messages.append({"role": "assistant", "content": gemini_response.text})
-    elif model_selection == "Claude 3.5":
-        with st.chat_message("assistant"):
-            anthropic_response = client.completions.create(
-                model=model_mapping[model_selection],
-                prompt=prompt,
-                max_tokens_to_sample=300,  # You can adjust the max tokens as needed
-            )
-            st.markdown(anthropic_response['completion'])
-            st.session_state.messages.append({"role": "assistant", "content": anthropic_response['completion']})
 
 hide_st_style = """
             <style>
