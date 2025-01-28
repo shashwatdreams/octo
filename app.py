f0957c3fb3a8f4356a41f960bf66b299e615d34e
Shashwat Sharma <kush.shashwat@outlook.com>
2025-01-28 04:20:27 -0500

    openAI client

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ st.set_page_config(
     }
 )
 
+client = OpenAI()
+
 def check_password():
     def password_entered():
         if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
